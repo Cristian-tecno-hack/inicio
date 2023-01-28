0cccd472c7c64b20cb65db245b770dbc22382f89
Cristian-tecno-hack <111001170+Cristian-tecno-hack@users.noreply.github.com>
2023-01-28 12:19:29 -0600

    lll
    a

---
+++ sweetAlert.js
@@ -0,0 +1,60 @@
+Swal.fire({
+	title:'Bienveido a Tecno-Hack',
+	text:'espero disfrutes de mi sitio web  ',
+	// html:
+	icon:'info',
+	confirmButtonText:'Aceptar',
+	footer:'<h5 style="color:blue";>Sitio Web Creado Por Cristian Cardona</h5>',
+	 width:'90%',
+	// padding:
+	// background:
+	// grow:
+	// backdrop:
+	//timer:5000,
+	// timerProgressBar:
+	// toast:
+	// position:
+	// allowOutsideClick:
+	// allowEscapeKey:
+	// allowEnterKey:
+	// stopKeydownPropagation:
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:
+	// 	cancelButton:
+	// 	footer:	
+
+	// showConfirmButton:
+	// confirmButtonColor:
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	// imageUrl:
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+});
